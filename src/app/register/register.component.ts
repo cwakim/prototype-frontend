409560be1dfa03700dd20af83f5d2f294cccfbd1
Charbel Wakim <charbel.wakim@keeward.com>
2019-12-28 21:37:24 +0200

    Create the register componenets

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+// register.component.ts
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class RegisterComponent implements OnInit {
+   constructor(private usersService: UsersService, private _router: Router) { }
+
+   ngOnInit() {
+   }
+
+   onClickSubmit(formData) {
+     this.usersService.registerUser(
+       formData.name,
+       formData.email,
+       formData.password,
+       formData.c_password
+     ).subscribe((res : any)=>{
+       localStorage.setItem('token', res.data.token);
+       this._router.navigate(['/home']);
+     });
+   }
+}
